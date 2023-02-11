2c9faabe531f959814808a5fcfe83779007e3439
Ubuntu <ubuntu@ip-172-31-10-126.eu-west-2.compute.internal>
2023-02-11 20:52:25 +0000

    async execution

---
+++ run.sh
@@ -0,0 +1,4 @@
+export FLASK_DEBUG=True
+export FLASK_APP=server
+
+/usr/bin/python3.8 -m flask run
--- server.py
+++ server.py
@@ -1,22 +1,26 @@
 from flask import Flask, request
+import whisper
+import whisperx
 from flask_restful import Resource, Api
-import asyncio
+import torch
+import threading
 
 from whisperx.transcribe import transcribe
 import os.path
 
 app = Flask(__name__)
 api = Api(app)
+working = False
 
 @app.route('/transcribe/')
-def hello():
-    return 'Hello, World!'
+
 
 class TranscribeHandler(Resource):
+    
     def post(self):
-
+        global model
         transcriptionRequest = request.json
-        print("request is:", transcriptionRequest)
+        print("request is:", transcriptionRequest, working)
         pathToFile = transcriptionRequest.get('pathToFile')
         if pathToFile is None:
             return {
@@ -30,7 +34,7 @@ class TranscribeHandler(Resource):
 
         if os.path.isfile(pathToFile) == False:
             return {
-                'message': f"the file at path {pathToFile} was not found"
+                'message': f"the file at path '{pathToFile}' was not found"
             }, 404
 
         outputPath = transcriptionRequest.get('outputPath')
@@ -46,12 +50,52 @@ class TranscribeHandler(Resource):
                 'message': f"'modelSize' must be one of ['small', 'medium', 'large', 'large-v2']"
             }, 400
 
+        if working:
+            return {
+                'message': f"a video is currently being processed, try again later"
+            }, 529
 
-        task1 = asyncio.create_task(
-            transcribe(modelSize, pathToFile, True))
+        print('creating task for request')
+
+        transcribe_thread = threading.Thread(target=workOnce, name="Transcriber Function", args=[pathToFile, outputPath, modelSize])
+        transcribe_thread.start()
+
         return {}, 200
 
     pass
 
+def workOnce(audio_file, outputPath, modelSize):
+    global working
+    working = True
+    print('working')
+    work(audio_file, outputPath, modelSize)
+    print('done working')
+    working = False
+
+def work1(audio_file, outputPath, modelSize):
+    transcribe(model, audio_file)
+
+def work(audio_file, outputPath, modelSize):
+    device = "cuda" 
+    # transcribe with original whisper
+    model = whisperx.load_model(modelSize, device)
+    result = model.transcribe(audio_file)
+
+    print(result["segments"]) # before alignment
+
+    # load alignment model and metadata
+    model_a, metadata = whisperx.load_align_model(language_code=result["language"], device=device)
+
+    # align whisper output
+    result_aligned = whisperx.align(result["segments"], model_a, metadata, audio_file, device)
+
+    print("SEGMENTS", result_aligned["segments"], "\n\n") # after alignment
+    print("WORD_SEGMENTS", result_aligned["word_segments"],"\n\n") # after alignment
+
+
+
+print('\n\n\nloading Whisper model...')
+model = whisper.load_model('medium').cuda()
+print("model loaded. CUDA available:", torch.cuda.is_available())
 api.add_resource(TranscribeHandler, '/transcribe')
 
