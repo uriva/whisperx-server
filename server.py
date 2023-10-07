7da90a79af7d81e3a323a49628e5674650f61666
uri <uri.valevski@gmail.com>
2023-10-07 20:08:34 +0300

    add sync mode and better flags handling, format

--- server.py
+++ server.py
@@ -1,92 +1,111 @@
+import argparse
+import logging
+import os.path
+import threading
+
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Api, Resource
+from waitress import serve
 
 from whisperx.worker import Worker
-
-import logging
-import threading
-import os.path
 
 app = Flask(__name__)
 api = Api(app)
 
-@app.route('/transcribe/')
 
-
+@app.route("/transcribe/")
 class TranscribeHandler(Resource):
-    
     def post(self):
         global model
-        transcriptionRequest = request.json
+        transcription_request = request.json
 
-        audio_path = transcriptionRequest.get('audioPath')
-        output_dir = transcriptionRequest.get('outputDir')
-        task = transcriptionRequest.get('task')
+        audio_path = transcription_request.get("audioPath")
+        output_dir = transcription_request.get("outputDir")
+        task = transcription_request.get("task")
+        sync = transcription_request.get("sync")
         if audio_path is None:
-            return {
-                'message': "'audioPath' key is missing"
-            }, 400        
+            return {"message": "'audioPath' key is missing"}, 400
 
-        if str(audio_path) == False:
-            return {
-                'message': "'pathToFile' value is not a string"
-            }, 400
+        if not str(audio_path):
+            return {"message": "'pathToFile' value is not a string"}, 400
 
-        if os.path.isfile(audio_path) == False:
+        if not os.path.isfile(audio_path):
+            return {"message": f"the file at path '{audio_path}' was not found"}, 404
+
+        if task is not None and str(task) not in ["translate", "transcribe"]:
             return {
-                'message': f"the file at path '{audio_path}' was not found"
-            }, 404
-        
-        if task is not None and str(task) not in ['translate', 'transcribe']:
-            return {
-                'message': f"the value of task is not valid: '{audio_path}'. Must be one of [translate, transcribe]"
+                "message": f"the value of task is not valid: '{audio_path}'. Must be one of [translate, transcribe]"
             }, 404
 
         if task is None:
-            logging.info('"task" query parameter not provided - using the default \'transcribe\'')
-            task = 'transcribe'
+            logging.info(
+                "\"task\" query parameter not provided - using the default 'transcribe'"
+            )
+            task = "transcribe"
 
         if output_dir is None:
             output_dir = os.path.dirname(os.path.abspath(audio_path))
 
         logging.info(f"request received: {task} [{audio_path} -> {output_dir}]")
 
-        if worker.isBusy():
+        if worker.is_busy():
             return {
-                'message': f"a video is currently being processed, try again later"
+                "message": "a video is currently being processed, try again later"
             }, 529
 
-        transcribe_thread = threading.Thread(target=worker.work, name="Transcriber Function", args=[audio_path, output_dir, task])
+        transcribe_thread = threading.Thread(
+            target=worker.work,
+            name="Transcriber Function",
+            args=[audio_path, output_dir, task],
+        )
         transcribe_thread.start()
-
+        if sync:
+            transcribe_thread.join()
         return {}, 200
 
-    pass
 
 format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=format, level=logging.INFO,
-                    datefmt="%H:%M:%S")
-
-device = os.environ.get('WHISPER_DEVICE')
-if device == None:
-    device = 'cpu'
-
-torch_threads = int(os.getenv('TORCH_THREADS', 1))
-if torch_threads == None:
-    torch_threads = 1
-
-modelSize = os.environ.get('WHISPER_MODEL')
-if modelSize == None:
-    modelSize = 'large-v2'
-if modelSize not in ['small', 'medium', 'large', 'large-v2']:
-    logging.error("invalid WHISPER_MODEL value. Must be one of ['small', 'medium', 'large', 'large-v2']")
-worker = Worker(modelSize, device, torch_threads)
+logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 
 
+parser = argparse.ArgumentParser(description="Process some flags.")
 
-api.add_resource(TranscribeHandler, '/transcribe')
+parser.add_argument(
+    "--device",
+    default=os.environ.get("WHISPER_DEVICE", "cpu"),
+    help="Specify the device (default: cpu)",
+)
+
+parser.add_argument(
+    "--torch_threads",
+    type=int,
+    default=int(os.getenv("TORCH_THREADS", 1)),
+    help="Specify the number of Torch threads (default: 1)",
+)
+
+parser.add_argument(
+    "--model_size",
+    default=os.environ.get("WHISPER_MODEL", "large-v2"),
+    choices=["small", "medium", "large", "large-v2"],
+    help="Specify the model size (default: large-v2)",
+)
+
+args = parser.parse_args()
+
+device = args.device
+torch_threads = args.torch_threads
+model_size = args.model_size
+
+if model_size not in ["small", "medium", "large", "large-v2"]:
+    logging.error(
+        "invalid WHISPER_MODEL value. Must be one of ['small', 'medium', 'large', 'large-v2']"
+    )
+
+worker = Worker(model_size, device, torch_threads)
+
+
+api.add_resource(TranscribeHandler, "/transcribe")
 
 
 if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    serve(app, host="localhost", port=8080)
--- whisperx/worker.py
+++ whisperx/worker.py
@@ -1,16 +1,18 @@
-import logging
-from . import load_model
-from .transcribe import transcribe
-from .alignment import load_align_model, align
-from .utils import write_txt, write_srt
-import time
-import os
-import torch
 import json
+import logging
+import os
+import time
 from datetime import datetime
 
+import torch
 
-class Worker():
+from . import load_model
+from .alignment import align, load_align_model
+from .transcribe import transcribe
+from .utils import write_srt, write_txt
+
+
+class Worker:
     _model = None
     _working_status = False
     _device = "cpu"
@@ -19,9 +21,11 @@ class Worker():
         self._model_size = model_size
         self._device = device
         torch.set_num_threads(torch_threads)
-        logging.info(f"initialized worker with device={device}, torch_threads={torch_threads}, model_size={model_size}")
+        logging.info(
+            f"initialized worker with device={device}, torch_threads={torch_threads}, model_size={model_size}"
+        )
 
-    def isBusy(self):
+    def is_busy(self):
         return self._working_status
 
     def work(self, audio_path, output_dir, task):
@@ -31,23 +35,18 @@ class Worker():
 
         self.working_status = True
         model = self.model
-
-        ######## REAL WORK IS BEING DONE HERE ###########
         self.work_on_file(model, audio_path, output_dir, task)
-        #################################################
-
         self.working_status = False
-        return
 
     def work_on_file(self, model, audio_path, output_dir, task):
         logging.info(f"Will {task} {audio_path} with {self._device}...")
         start = time.time()
 
         args = {
-            "language": None, 
-            "task": str(task), 
-            "verbose": False, 
-            }
+            "language": None,
+            "task": str(task),
+            "verbose": False,
+        }
 
         if self._device == "cpu":
             args["fp16"] = False
@@ -59,37 +58,56 @@ class Worker():
 
         logging.info(f"Alligning {audio_path}...")
         align_model, align_metadata = load_align_model(lan, self._device)
-        result_aligned = align(result["segments"], align_model, align_metadata, audio_path, self._device,
-                                extend_duration=2, start_from_previous=True, interpolate_method="nearest")
+        result_aligned = align(
+            result["segments"],
+            align_model,
+            align_metadata,
+            audio_path,
+            self._device,
+            extend_duration=2,
+            start_from_previous=True,
+            interpolate_method="nearest",
+        )
 
         os.makedirs(output_dir, exist_ok=True)
 
+        filename = os.path.splitext(audio_path)[0]
+
         # save TXT
-        with open(os.path.join(output_dir, "transcript.txt"), "w", encoding="utf-8") as txt:
+        with open(
+            os.path.join(output_dir, f"{filename}.txt"), "w", encoding="utf-8"
+        ) as txt:
             write_txt(result_aligned["segments"], file=txt)
 
         # save SRT
-        with open(os.path.join(output_dir,  "subtitles.srt"), "w", encoding="utf-8") as srt:
+        with open(
+            os.path.join(output_dir, f"{filename}.srt"), "w", encoding="utf-8"
+        ) as srt:
             write_srt(result_aligned["segments"], file=srt)
 
         # save metadata JSON
         end = time.time()
-        logging.info(f"Finished processing {audio_path} within {round(end - start)} seconds (model: {self._model_size})")
+        logging.info(
+            f"Finished processing {audio_path} within {round(end - start)} seconds (model: {self._model_size})"
+        )
 
-        now = datetime.now() # current date and time
+        now = datetime.now()  # current date and time
         date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
 
         metadata = {
-            "language": lan, 
-            "task": "transcribe", 
-            "source": audio_path, 
-            "whisper_model": self._model_size, 
+            "language": lan,
+            "task": "transcribe",
+            "source": audio_path,
+            "whisper_model": self._model_size,
             "duration": f"{round(end - start)} seconds",
             "finished_at": date_time,
-            }
-        with open(os.path.join(output_dir,  "transcription_metadata.json"), "w", encoding="utf-8") as fd:
+        }
+        with open(
+            os.path.join(output_dir, f"{filename}.json"),
+            "w",
+            encoding="utf-8",
+        ) as fd:
             json.dump(metadata, fd)
-
 
     def _get_working_status(self):
         return self._working_status
@@ -100,13 +118,13 @@ class Worker():
     working_status = property(
         fget=_get_working_status,
         fset=_set_working_status,
-        doc="Whether the worker is currently working or not."
+        doc="Whether the worker is currently working or not.",
     )
 
     @property
     def model(self):
         if not self._model:
-            logging.info(f'lazy loading Whisper model... (size: {self._model_size})')
+            logging.info(f"lazy loading Whisper model... (size: {self._model_size})")
             self._model = load_model(self._model_size, device=self._device)
             logging.info("Whisper model loaded successfully!")
         return self._model
@@ -127,6 +145,6 @@ class Worker():
         # result_aligned["segments"] = result_segments
         # result_aligned["word_segments"] = word_segments
 
+
 format = "%(asctime)s: %(message)s"
-logging.basicConfig(format=format, level=logging.INFO,
-                    datefmt="%H:%M:%S")
+logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
