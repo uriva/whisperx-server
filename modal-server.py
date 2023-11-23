a952f69e199e75ff5e6b049363ca7a03edaf622f
uri <uri.valevski@gmail.com>
2023-11-23 04:48:41 +0200

    add modal file

---
+++ modal-server.py
@@ -0,0 +1,51 @@
+import logging
+from typing import Dict
+
+import modal
+from whisperx.alignment import align, load_align_model
+
+from src import worker
+
+stub = modal.Stub("whisperx-service")
+_model_version = "large-v2"
+params = (_model_version, "cuda", 1, "float16")
+_example_video_file = "https://www2.cs.uic.edu/~i101/SoundFiles/taunt.wav"
+
+
+_image = (
+    modal.Image.debian_slim()
+    .apt_install("ffmpeg")
+    .apt_install("git")
+    .pip_install_from_requirements("./requirements.txt")
+    .run_commands(
+        f'whisperx --model {_model_version} "{_example_video_file}"',
+        gpu="any",
+    )
+)
+
+
+@stub.function(image=_image, gpu="any")
+@modal.web_endpoint(method="POST")
+def _transcribe(request: Dict):
+    language = request["language"]
+    task = request["task"]
+    audio_path = request["audio_path"]
+
+    model = worker.setup_model(*params)
+
+    logging.info(f"{task} {audio_path} {model.device} {language}")
+    try:
+        result = model.transcribe(audio_path, language=language, task=task)
+        align_model, align_metadata = load_align_model(result["language"], model.device)
+        return worker.write_srt(
+            align(
+                result["segments"],
+                align_model,
+                align_metadata,
+                audio_path,
+                model.device,
+            )["segments"]
+        )
+    except Exception as e:
+        logging.error(e)
+        return None
